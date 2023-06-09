e36a3852b2380f035ea5449b28b8aa7191b8f03f
hyunho <mizzking75@gmail.com>
2023-06-10 01:09:03 +0900

    Initial Commit

---
+++ Rebound Journal/Rebound_JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rebound_JournalApp.swift
+//  Rebound Journal
+//
+//  Created by hyunho lee on 2023/06/10.
+//
+
+import SwiftUI
+
+@main
+struct Rebound_JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
