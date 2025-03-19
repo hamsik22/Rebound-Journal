d7252f4e1dd6cc3ac124df8706c798db7890ec63
HwangSukHyeon <payback9488@gmail.com>
2025-03-19 16:41:28 +0900

    [File&Folder]: 작업 환경 및 폴더 생성

---
+++ Rebound Journal/Hamsik/View/EmotionInputView.swift
@@ -0,0 +1,18 @@
+//
+//  RecordFeelingView.swift
+//  Rebound Journal
+//
+//  Created by 황석현 on 3/19/25.
+//
+
+import SwiftUI
+
+struct RecordFeelingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecordFeelingView()
+}
---
+++ Rebound Journal/Hamsik/View/SelectShootTypeView.swift
@@ -0,0 +1,18 @@
+//
+//  SelectShootTypeView.swift
+//  Rebound Journal
+//
+//  Created by 황석현 on 3/19/25.
+//
+
+import SwiftUI
+
+struct SelectShootTypeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectShootTypeView()
+}
