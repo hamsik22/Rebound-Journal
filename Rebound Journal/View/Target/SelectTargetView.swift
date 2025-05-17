33d92ca104b5b77817055db5d3ff1faaabef8d3e
HwangSukHyeon <payback9488@gmail.com>
2025-05-18 03:24:24 +0900

    [Chore]: 줄 바꿈

--- Rebound Journal/View/Target/SelectTargetView.swift
+++ Rebound Journal/View/Target/SelectTargetView.swift
@@ -17,57 +17,57 @@ struct SelectTargetView: View {
     var subGoals: [SubGoalData]
     
     var body: some View {
-            VStack {
-                // 질문
-                VStack(alignment: .leading, spacing: 8) {
-                    HStack {
-                        Text("어떤 슛을 남겨볼까요?")
-                            .font(.system(size: 25).bold())
-                        Spacer()
-                    }
-                    HStack {
-                        Text("목표에 대한 슛인가요?")
-                            .font(.system(size: 18))
-                            .opacity(0.5)
-                        Spacer()
-                    }
+        VStack {
+            // 질문
+            VStack(alignment: .leading, spacing: 8) {
+                HStack {
+                    Text("어떤 슛을 남겨볼까요?")
+                        .font(.system(size: 25).bold())
+                    Spacer()
                 }
-                .padding(.vertical)
-                
-                // 목표 리스트
-                VStack(alignment: .leading, spacing: 8) {
-                    ForEach(subGoals, id: \.self) { item in
-                        if let text = item.goalText {
-                            HStack {
-                                Text(text)
-                                    .padding()
-                                Spacer()
-                            }
-                            .frame(maxWidth: .infinity)
-                            .background(.gray.opacity(0.5))
-                            .clipShape(.rect(cornerRadius: 12))
-                        }
-                    }
+                HStack {
+                    Text("목표에 대한 슛인가요?")
+                        .font(.system(size: 18))
+                        .opacity(0.5)
+                    Spacer()
                 }
-                
-                Spacer()
-                
-                Button(action: {
-                    print("다음 화면으로 이동")
-                    viewModel.currentStep = .shoot
-                }) {
-                    Text("건너뛰기")
-                        .font(.system(size: 18, weight: .bold))
-                        .padding()
-                        .frame(maxWidth: .infinity)
-                        .background(Color.accentColor)
-                        .foregroundColor(.white)
-                        .clipShape(.rect(cornerRadius: 90))
-                }
-                .disabled(false)
-                .animation(.easeInOut, value: 1)
             }
-            .padding()
+            .padding(.vertical)
+            
+            // 목표 리스트
+            VStack(alignment: .leading, spacing: 8) {
+                ForEach(subGoals, id: \.self) { item in
+                    if let text = item.goalText {
+                        HStack {
+                            Text(text)
+                                .padding()
+                            Spacer()
+                        }
+                        .frame(maxWidth: .infinity)
+                        .background(.gray.opacity(0.5))
+                        .clipShape(.rect(cornerRadius: 12))
+                    }
+                }
+            }
+            
+            Spacer()
+            
+            Button(action: {
+                print("다음 화면으로 이동")
+                viewModel.currentStep = .shoot
+            }) {
+                Text("건너뛰기")
+                    .font(.system(size: 18, weight: .bold))
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.accentColor)
+                    .foregroundColor(.white)
+                    .clipShape(.rect(cornerRadius: 90))
+            }
+            .disabled(false)
+            .animation(.easeInOut, value: 1)
+        }
+        .padding()
     }
 }
 
