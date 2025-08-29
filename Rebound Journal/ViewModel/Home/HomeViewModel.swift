eddc517ac5b4580709416693c06713bdf633e091
HwangSukHyeon <payback9488@gmail.com>
2025-08-29 15:06:26 +0900

    ➕ :: HomeViewModel 추가

---
+++ Rebound Journal/ViewModel/Home/HomeViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  HomeViewModel.swift
+//  Rebound Journal
+//
+//  Created by 황석현 on 8/29/25.
+//
+
+import Foundation
+
+@MainActor
+final class HomeViewModel: ObservableObject {
+    
+    
+}
